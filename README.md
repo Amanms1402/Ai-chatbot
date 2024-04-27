24ca8dc0ee7f3bbea73815de40b0c7cd8af4b973
Amanms1402 <119517551+Amanms1402@users.noreply.github.com>
2024-04-27 23:25:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chat - AI
+
+AI Chat App using Generative Language Client API
+
+[chat-ai](https://ai.visheshpandey.com)
+
+## Setup Procedure
+
+- Copy .env.example to .env
+- Get API key from: [api-key](https://aistudio.google.com/app/apikey)
+- `npm install`
+- `npm run dev`
---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [generatingAnswer, setGeneratingAnswer] = useState(false);
+
+  async function generateAnswer(e) {
+    setGeneratingAnswer(true);
+    e.preventDefault();
+    setAnswer("Loading your answer... \n It might take upto 10 seconds");
+    try {
+      const response = await axios({
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${
+          import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT
+        }`,
+        method: "post",
+        data: {
+          contents: [{ parts: [{ text: question }] }],
+        },
+      });
+
+      setAnswer(
+        response["data"]["candidates"][0]["content"]["parts"][0]["text"]
+      );
+    } catch (error) {
+      console.log(error);
+      setAnswer("Sorry - Something went wrong. Please try again!");
+    }
+
+    setGeneratingAnswer(false);
+  }
+
+  return (
+    <>
+      <div className="bg-white h-screen p-3">
+        <form
+          onSubmit={generateAnswer}
+          className="w-full md:w-2/3 m-auto text-center rounded bg-gray-50 py-2"
+        >
+          <a href="https://github.com/Vishesh-Pandey/chat-ai" target="_blank">
+            <h1 className="text-3xl text-center">Chat AI</h1>
+          </a>
+          <textarea
+            required
+            className="border rounded w-11/12 my-2 min-h-fit p-3"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            placeholder="Ask anything"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-blue-300 p-3 rounded-md hover:bg-blue-400 transition-all duration-300"
+            disabled={generatingAnswer}
+          >
+            Generate answer
+          </button>
+        </form>
+        <div className="w-full md:w-2/3 m-auto text-center rounded bg-gray-50 my-1">
+          <ReactMarkdown className="p-3">{answer}</ReactMarkdown>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
